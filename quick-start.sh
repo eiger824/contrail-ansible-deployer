9330254696324430dca1abdd6516d3552303f60e
Santiago Pagola <santiago.pagola@ericsson.com>
2019-04-12 15:36:23 +0200

    working instances

--- quick-start.sh
+++ quick-start.sh
@@ -2,6 +2,38 @@
 
 # Custom quick guide for the impatient
 
+banner()
+{
+    local cols
+    local msg 
+    local diff
+    cols=$(tput cols)
+    msg="$@"
+    echo -n "+"
+    for i in $(seq 1 $(( $cols - 2 ))); do
+        echo -n "-"
+    done
+    echo -ne "+\n|"
+    diff=$(( $cols - ${#msg} - 2 ))
+    start=$(( $diff / 2 ))
+    for i in $(seq 1 $start); do
+        echo -n " "
+    done
+    echo -n "${msg}"
+    rend=$start
+    if [ $(( $cols % 2 )) -ne 0 ]; then
+        ((rend++))
+    fi
+    for i in $(seq 1 $rend); do
+        echo -n " "
+    done
+    echo -ne "|\n+"
+    for i in $(seq 1 $(( $cols - 2 ))); do
+        echo -n "-"
+    done
+    echo "+"
+}
+
 check_err()
 {
     if [ $1 -ne 0 ]; then
@@ -10,23 +42,27 @@ check_err()
     fi
 }
 
-if [ $UID -ne 0 ]; then
-    echo "Run this script as root." >&2
-    exit 1
-fi
+# if [ $UID -ne 0 ]; then
+#     echo "Run this script as root." >&2
+#     exit 1
+# fi
 
 # Provision instances first - create KVMs
-# ansible-playbook -e orchestrator=openstack -i inventory/ playbooks/provision_instances.yml
-# check_err $?
+banner "Provisioning instances!"
+ansible-playbook -e orchestrator=openstack -i inventory/ playbooks/provision_instances.yml
+check_err $?
 
 # Configure these machines
+banner "Configuring instances!"
 ansible-playbook -e orchestrator=openstack -i inventory/ playbooks/configure_instances.yml
 check_err $?
 
 # Install Openstack Kolla
+banner "Installing Openstack!"
 ansible-playbook -e orchestrator=openstack -i inventory/ playbooks/install_openstack.yml
 check_err $?
 
 # Deploy Contrail
+banner "Installing Tungsten Fabric!"
 ansible-playbook -e orchestrator=openstack -i inventory/ playbooks/install_contrail.yml
 check_err $?
--- tf-scripts/tf-status-vms.sh
+++ tf-scripts/tf-status-vms.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ $UID -ne 0 ]; then
-    echo "Run this script as the superuser" >&2
-    exit 1
-fi
+source common.src
 
+test_if_root
+
+echo "$(pad_str 25 "Name")Status"
+echo "$(pad_str 25 "====")======"
 for vm in contrail-controller-01 \
     contrail-compute-01 \
     contrail-compute-02; do
     vm_status=$(virsh domstate ${vm} | head -1)    
-    echo "\"${vm}\" ==> ${vm_status}"
+    echo "$(pad_str 25 ${vm})${vm_status}"
 done
 
